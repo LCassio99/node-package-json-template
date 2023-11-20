4b5eed53a82193896b0442c296cc023401d87775
Luciano Brito <lucianocassio66@gmail.com>
2023-11-20 20:12:30 -0300

    template-aula-node-packagejson

---
+++ pratica2.js
@@ -0,0 +1,11 @@
+import {countries} from './countries.js'
+
+const findCountry = (inputCountry) =>{
+    const pais  = countries.find((pais) =>{
+        return pais.name.toLowerCase() === inputCountry.toLowerCase()
+    })
+
+    return pais
+}
+
+console.log(findCountry(process.argv[2]))
